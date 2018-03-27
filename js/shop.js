385f51a6cde46d8031e71acc37f9316a3f882778
yanadyukova <yana.a.dukova@gmail.com>
2018-03-27 19:49:38 +0300

    реализовала добавление/удаление артефактов

--- js/shop.js
+++ js/shop.js
@@ -2,16 +2,23 @@
 
 (function () {
     var shopElement = document.querySelector('.setup-artifacts-shop');
+    var artifactsElement = document.querySelector('.setup-artifacts');
+    var basket = document.querySelector('.setup-artifacts-delete');
     var draggedItem = null;
 
     shopElement.addEventListener('dragstart', function (evt) {
+        if (evt.target.tagName.toLowerCase() === 'img') {
+            draggedItem = evt.target.cloneNode(true);
+            evt.dataTransfer.setData('text/plain', evt.target.alt);
+        }
+    });
+
+    artifactsElement.addEventListener('dragstart', function (evt) {
         if (evt.target.tagName.toLowerCase() === 'img') {
             draggedItem = evt.target;
             evt.dataTransfer.setData('text/plain', evt.target.alt);
         }
     });
-
-    var artifactsElement = document.querySelector('.setup-artifacts');
 
     artifactsElement.addEventListener('dragover', function (evt) {
         evt.preventDefault();
@@ -21,13 +28,21 @@
     artifactsElement.addEventListener('drop', function (evt) {
         evt.target.style.outline = '';
         evt.target.style.backgroundColor = '';
-        evt.target.appendChild(draggedItem.cloneNode(true));
+
+        if (evt.target.children.length === 0 && !(evt.target.tagName.toLowerCase() === 'img')) {
+            evt.target.appendChild(draggedItem);
+        }
+
         evt.preventDefault();
     });
 
     artifactsElement.addEventListener('dragenter', function (evt) {
-        evt.target.style.outline = '2px dashed red';
-        evt.target.style.backgroundColor = 'lightyellow';
+
+        if (evt.target.children.length === 0 && !(evt.target.tagName.toLowerCase() === 'img')) {
+            evt.target.style.outline = '2px dashed red';
+            evt.target.style.backgroundColor = 'lightyellow';
+        }
+
         evt.preventDefault();
     });
 
@@ -36,4 +51,16 @@
         evt.target.style.backgroundColor = '';
         evt.preventDefault();
     });
+
+    basket.addEventListener('dragover', function (evt) {
+        evt.preventDefault();
+        return false;
+    });
+
+    basket.addEventListener('drop', function (evt) {
+        evt.target.appendChild(draggedItem);
+        evt.target.removeChild(draggedItem);
+        evt.preventDefault();
+    });
+
 })();
